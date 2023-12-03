31e028fe2e8d6d8c306be54369caba86b4756bc9
Mladen Popadic <mladen.popadic.4@gmail.com>
2023-12-03 13:00:27 +0100

    fix typo userID

--- handlers/getUser.go
+++ handlers/getUser.go
@@ -10,7 +10,7 @@ import (
 func (h handler) GetUser(c *gin.Context) {
 	id := c.Params.ByName("id")
 	var user models.User
-	if err := h.DB.First(&user, 10).Error; err != nil {
+	if err := h.DB.First(&user, id).Error; err != nil {
 		c.JSON(http.StatusOK, gin.H{"user_id": id, "status": "no value"})
 	} else {
 		c.JSON(http.StatusOK, gin.H{"user": user})
